ef905fba005b3a84ef2ab8833f0fbbbe564b6757
Marcin Kos <marcin.kos.krk@gmail.com>
2022-05-22 10:29:17 +0200

    login component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import React from 'react';
-import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 import Login from '../Auth/Login';
 import Register from '../Auth/Register';
 import Header from '../Header/Header';
@@ -8,37 +9,42 @@ import useCookie from '../hooks/useCookies';
 import Profile from '../Profile/Profile';
 import Train from '../Train/Train';
 
-import'./App.scss';
+import './App.scss';
 import { navigate } from '@storybook/addon-links';
 
-
 function App() {
-    const [cookie, setCookie] = useCookie('token_auth');
+    // const [cookie, setCookie] = useCookie('token_auth');
 
-    useEffect(() => {
-        if (cookie === undefined) {
-            navigate('/login');
-        }
-    }, [])
+    // useEffect(() => {
+    //     if (cookie === undefined) {
+    //         navigate('/login');
+    //     }
+    // }, [])
 
     return (
         <BrowserRouter>
-            <Header/>
-            <Routes>
-                {cookie ? 
-                <>
-                <Route path="/" element={<Link to="/profile">Profile</Link>}/>
-                <Route path="/profile" element={<Profile/>}/>
-                <Route path="/train" element={<Train/>}/>
-                </> 
-                : 
-                <>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/register" element={<Register/>}/>
-                </>}
-            </Routes>
+            <Header />
+            <Container>
+                <Row>
+                    <Col>
+                        <Routes>
+                            {/* {cookie ? 
+                            <> */}
+                            <Route path="/" element={<Link to="/profile">Profile</Link>} />
+                            <Route path="/profile" element={<Profile />} />
+                            <Route path="/train" element={<Train />} />
+                            {/* </> 
+                            : 
+                            <> */}
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/register" element={<Register />} />
+                            {/* </>} */}
+                        </Routes>
+                    </Col>
+                </Row>
+            </Container>
         </BrowserRouter>
-      );
+    );
 }
 
 export default App;
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,10 +1,52 @@
-import React from "react";
-
+import React, { useState } from 'react';
+import { Button, Card, Form } from 'react-bootstrap';
 
 function Login(props) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [remeber, setRemeber] = useState(false);
+
+    function handleEmail(event) {
+        setEmail(event.target.value);
+    }
+
+    function handlePassword(event) {
+        setPassword(event.target.value);
+    }
+
+    function handleRemember(event) {
+        setRemeber(event.target.checked);
+    }
+
+    function submitForm(event) {
+        event.preventDefault(); //prevent page reloading
+        console.log(email, password, remeber)
+    }
+
     return (
-        <div>Login</div>
-    )
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>Login:</Card.Title>
+                    <Form onSubmit={submitForm} className={"d-flex flex-column"}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" value={email} onChange={handleEmail}/>
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password"  value={password} onChange={handlePassword}/>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Remeber me" checked={remeber} onChange={handleRemember}/>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Login
+                        </Button>
+                    </Form>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default Login;
